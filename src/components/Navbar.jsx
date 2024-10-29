28b7c386d6829efe628021b5aa1ddb19a604c746
joek-yo <jkihiu129@gmail.com>
2024-10-29 20:29:43 +0300

    Add school name to Navbar and update styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 // Navbar.jsx - Component for the Navigation Bar
 
-import './Navbar.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav>
+      <h1>Tommie Fulp Adventist Academy $ Junior High School</h1>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About Us</Link></li>
